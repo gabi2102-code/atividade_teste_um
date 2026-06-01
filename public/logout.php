3078a698209965b650a3768a0e525c564eec1f8b
gabi2102-code <gabriela_b_ferreira@estudante.sesisenai.org.br>
2026-06-01 10:03:28 -0300

    doc- Adicionei comentarios sobre a estrutura da pagina de logout

--- public/logout.php
+++ public/logout.php
@@ -1,6 +1,10 @@
 <?php
+ // Inicia a sessão para ter acesso aos dados armazenados nela
     session_start();
+// Remove todos os dados da sessão atual
     session_destroy();
+// Redireciona o usuário para a página de login    
     header("Location: ../index.php");
+// Encerra a execução do script
     exit();
 ?>
