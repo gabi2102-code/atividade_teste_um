87576fea335f8ef13f9f0687cedc45a48c659c06
gabi2102-code <gabriela_b_ferreira@estudante.sesisenai.org.br>
2026-06-01 09:43:22 -0300

    doc- Adicionei alguns comentarios para o melhor entendimento do codigo

--- infra/db/script.sql
+++ infra/db/script.sql
@@ -1,11 +1,19 @@
+-- Cria um banco de dados chamado sistema_simples
 CREATE DATABASE sistema_simples;
 
+-- Seleciona o banco de dados que será utilizado
 USE sistema_simples;
 
+-- Cria a tabela usuario
 CREATE TABLE usuario (
+
+    -- Identificador único do usuário
     id INT AUTO_INCREMENT PRIMARY KEY,
+
+     -- Campo para armazenar o nome de usuário
     usuario VARCHAR(255) NOT NULL,
+     -- Campo para armazenar a senha
     senha VARCHAR(255) NOT NULL
 );
-
+-- Insere um usuário de teste na tabela
 INSERT INTO usuario (usuario, senha) VALUE ('admin','123');
